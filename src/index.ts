96300ae1b68a8a2aed2e63dcd4c171e2876a8629
Hatta <hattagithub@gmail.com>
2025-04-20 00:07:44 +0700

    feat: add server endpoint checking

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,19 @@ import db from "./utils/database";
 async function init() {
   try {
     const result = await db();
-    console.log("Database status: ", result)
+    console.log("Database status: ", result);
 
     const app = express();
     app.use(bodyParser.json());
 
     const PORT = 3000;
+
+    app.get("/", (req, res) => {
+      res.status(200).json({
+        message: "Server sedang berjalan",
+        data: null,
+      });
+    });
 
     app.use("/api", router);
 
