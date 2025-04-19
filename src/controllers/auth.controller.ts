747a0efa29a009546021464bbc7cf744714462c5
Hatta <hattagithub@gmail.com>
2025-04-20 00:04:13 +0700

    chore: setup deploy vercel

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,137 @@
+import { Request, Response } from "express";
+import * as Yup from "yup";
+
+import UserModel from "../models/user.model";
+import { encrypt } from "../utils/encryption";
+import { generateToken } from "../utils/jwt";
+import { IReqUser } from "../middlewares/auth.middleware";
+
+type TRegister = {
+  fullName: string;
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+type TLogin = {
+  identifier: string;
+  password: string;
+};
+
+const registerValidateSchema = Yup.object({
+  fullName: Yup.string().required(),
+  username: Yup.string().required(),
+  email: Yup.string().email().required(),
+  password: Yup.string().required(),
+  confirmPassword: Yup.string()
+    .required()
+    .oneOf([Yup.ref("password"), ""], "Password tidak sesuai"),
+});
+
+export default {
+  async register(req: Request, res: Response) {
+    const { fullName, username, email, password, confirmPassword } =
+      req.body as unknown as TRegister;
+
+    try {
+      await registerValidateSchema.validate({
+        fullName,
+        username,
+        email,
+        password,
+        confirmPassword,
+      });
+
+      const result = await UserModel.create({
+        fullName,
+        email,
+        username,
+        password,
+      });
+
+      res.status(200).json({
+        message: "Registrasi Sukses",
+        data: result,
+      });
+    } catch (error) {
+      const err = error as unknown as Error;
+      res.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
+  },
+
+  async login(req: Request, res: Response) {
+    const { identifier, password } = req.body as unknown as TLogin;
+
+    try {
+      // ambil data user berdasarkan "identifier" -> email dan username
+
+      const userByIdentifier = await UserModel.findOne({
+        $or: [
+          {
+            email: identifier,
+          },
+          {
+            username: identifier,
+          },
+        ],
+      });
+
+      if (!userByIdentifier) {
+        return res.status(403).json({
+          message: "User tidak ditemukan",
+          data: null,
+        });
+      }
+
+      // validasi password
+      const validatePassword: boolean =
+        encrypt(password) === userByIdentifier.password;
+
+      if (!validatePassword) {
+        return res.status(403).json({
+          message: "Password salah",
+          data: null,
+        });
+      }
+
+      const token = generateToken({
+        id: userByIdentifier._id,
+        role: userByIdentifier.role,
+      });
+
+      res.status(200).json({
+        message: "Login Sukses",
+        data: token,
+      });
+    } catch (error) {
+      const err = error as unknown as Error;
+      res.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
+  },
+
+  async me(req: IReqUser, res: Response) {
+    try {
+      const user = req.user;
+      const result = await UserModel.findById(user?.id);
+
+      res.status(200).json({
+        message: "Sukses mengambil data user",
+        data: result,
+      })
+      
+    } catch (error) {
+      const err = error as unknown as Error;
+      res.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import express from "express";
+import bodyParser from "body-parser";
 import router from "./routes/api";
+import db from "./utils/database";
 
-const app = express();
+async function init() {
+  try {
+    const result = await db();
+    console.log("Database status: ", result)
 
-const PORT = 3000
+    const app = express();
+    app.use(bodyParser.json());
 
-app.use('/api', router);
+    const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port http://localhost:${PORT}`);
-});
+    app.use("/api", router);
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+init();
